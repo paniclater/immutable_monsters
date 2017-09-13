2fe74fc4c67c75bdfcda748ae8b714d2de78f5f5
Ryan Moore <paniclater@gmail.com>
2017-09-12 20:11:20 -0600

    css cleanup

--- src/monsterCard.js
+++ src/monsterCard.js
@@ -1,10 +1,10 @@
 export function monsterCard(monsterSvg) {
   const card = document.createElement('div');
-  card.classList.add('monsterCard');
+  card.classList.add('monster-card');
 
   const svg = document.createElement('div');
   svg.innerHTML = monsterSvg;
-  svg.classList.add('monsterSvg');
+  svg.classList.add('monster-svg');
 
   const stats= document.createElement('div');
   stats.classList.add('stats');
@@ -31,7 +31,7 @@ export function monsterCard(monsterSvg) {
   levelBlockContainer.classList.add('level-block-container');
   for (var i = 0; i < 3; i++) {
     var l = document.createElement('div');
-    l.innerHTML = i;
+    l.innerHTML = i + 1;
     l.classList.add('level-block');
     levelBlockContainer.appendChild(l);
   }
