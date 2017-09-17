277b0cde9a6c5536c309484f8bd9d51e0582a1c0
Ryan Moore <paniclater@gmail.com>
2017-09-17 15:49:59 -0600

    svgElement -> svg

--- src/monsterData.js
+++ src/monsterData.js
@@ -6,8 +6,8 @@ import pointy from './svg/pointy.svg';
 import eyes from './svg/eyes.svg';
 
 export default [
-  { name: 'tippety', level: 1, type: 'spooketer',  svgElement: legs },
-  { name: 'floaty',  level: 1, type: 'spookemer',  svgElement: floaty },
-  { name: 'pointy',  level: 1, type: 'spookeezer', svgElement: pointy },
-  { name: 'gazey',   level: 1, type: 'spookaser',  svgElement: eyes }
+  { name: 'tippety', level: 1, type: 'spooketer',  svg: legs },
+  { name: 'floaty',  level: 1, type: 'spookemer',  svg: floaty },
+  { name: 'pointy',  level: 1, type: 'spookeezer', svg: pointy },
+  { name: 'gazey',   level: 1, type: 'spookaser',  svg: eyes }
 ];
