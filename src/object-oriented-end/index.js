a12b29134d2098ca29a7dbe3122b39a47af887aa
Ryan Moore <paniclater@gmail.com>
2017-09-18 12:41:23 -0600

    copying code just in cases

--- src/object-oriented-end/index.js
+++ src/object-oriented-end/index.js
@@ -7,11 +7,23 @@ import Trainer from './Trainer'
 import Battle from './Battle'
 import render from '../render'
 
-const me = new Trainer('Ryan', 'Good');
-const drake = new Trainer('Drake', 'Evil');
+//render('Catching Cute Monsters', 'With Object Oriented Patterns', []);
+
+const me = new Trainer('Ryan', 'Totally Evil');
+
+for (let i = 0; i < 4; i++) {
+  for (let j = 0; j < me.collection.monsters.length; j++) {
+    me.collection.monsters[j].levelUp();
+  }
+}
+
+render('Ryan\'s Collection', '', me.collection.monsters);
+
+
+const drake = new Trainer('Drake', 'Lawful Good');
+
+render('Drake\'s Collection', '', me.collection.monsters);
+
 const battle = new Battle(me, drake);
 
-render('TIME TO BATTLE', 'RYAN V DRAKE');
-//render('TIME TO BATTLE', 'RYAN\'S COLLECTION', me.collection.monsters)
-//render('TIME TO BATTLE', 'DRAKE\'S COLLECTION', drake.collection.monsters)
-render('RESULTS', battle.winner());
+render(battle.winner());
