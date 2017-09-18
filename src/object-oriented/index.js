fce99d99fe0bd65f7450bd731e3e6e0efcb1fdff
Ryan Moore <paniclater@gmail.com>
2017-09-17 22:06:31 -0600

    removing what will be added

--- src/object-oriented/index.js
+++ src/object-oriented/index.js
@@ -5,16 +5,4 @@ import '../style.css';
 import Monster from './Monster'
 import Trainer from './Trainer'
 import Battle from './Battle'
-import render from '../render'
-
-const me = new Trainer('Ryan', 'Good');
-const drake = new Trainer('Drake', 'Evil');
-const battle = new Battle(me, drake);
-
-me.collection.monsters.forEach(monster => monster.levelUp())
-me.collection.monsters.forEach(monster => monster.levelUp())
-me.collection.monsters.forEach(monster => monster.levelUp())
-render('TIME TO BATTLE', 'RYAN V DRAKE');
-//render('TIME TO BATTLE', 'RYAN\'S COLLECTION', me.collection.monsters)
-//render('TIME TO BATTLE', 'DRAKE\'S COLLECTION', drake.collection.monsters)
-render('RESULTS', battle.winner());
+import render from '../dom/render'
